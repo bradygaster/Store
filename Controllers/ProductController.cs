4d15dd5c0e9a63135c6625fcb0b7188c8966772b
bradygaster <bradyg@microsoft.com>
2021-01-08 01:03:41 -0800

    add a new feature in a new version

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,18 +15,26 @@ namespace Store.Controllers
         private readonly ILogger<ProductController> _logger;
         private IProductService _productService;
 
-        public ProductController(ILogger<ProductController> logger, 
+        public ProductController(ILogger<ProductController> logger,
             IProductService productService)
         {
             _logger = logger;
             _productService = productService;
         }
 
-        [ApiVersion( "1.0" )]
-        [HttpGet]
-        public async Task<IEnumerable<Product>> Get()
+        [ApiVersion("1.0")]
+        [ApiVersion("1.1")]
+        [HttpGet(Name = nameof(GetProducts))]
+        public async Task<IEnumerable<Product>> GetProducts()
         {
             return await _productService.GetProductsAsync();
+        }
+
+        [ApiVersion("1.1")]
+        [HttpPost(Name = nameof(CreateProduct))]
+        public Product CreateProduct([FromBody] Product product)
+        {
+            return product;
         }
     }
 }
